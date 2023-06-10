d22de11ff0aeade64e32a9cbe648fe524b838316
islam-24-03 <isabhnisa@gmail.com>
2023-06-10 11:53:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+//import React from 'react';
+//import ChildComponent from './ChildComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//class pComponent extends React.Component {
+//render() {
+//const data = "Hello World!";
+//return (
+//<div>
+//<ChildComponent message={data} />
+//</div>
+//);
+//}
+//}
+
+//export default pComponent;
+
+
+
+//import React from 'react';
+
+//class ChildComponent extends React.Component {
+//render() {
+//return (
+//   <div>
+//     <h1>{this.props.message}</h1>
+//     </div>
+//    );
+//}
+//}
+
+//export default ChildComponent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+import ChildComponent from './ChildComponent';
+
+class ParentComponent extends React.Component {
+  handleClick = () => {
+    console.log("Кликнули на кнопку в родительском компоненте");
+  }
+
+  render() {
+    return (
+      <div>
+        <ChildComponent handleClick={this.handleClick} />
+      </div>
+    );
+  }
 }
 
-export default App;
+
+
+
+
+
+class ChildComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.handleClick}>Нажми на меня</button>
+      </div>
+    );
+  }
+}
+
+export default ParentComponent;
+
